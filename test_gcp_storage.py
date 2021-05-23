fab9fea54b94b269b5a9c5dcade6fb209d824c70
Bharath <bharath@hooq.tv>
2021-05-23 21:53:25 +0800

    write testcase for gcp bucket

--- test_gcp_storage.py
+++ test_gcp_storage.py
@@ -1,5 +1,20 @@
 import pytest
 import pulumi
 
-def test_something():
-    return True
+
+class MyMocks(pulumi.runtime.Mocks):
+    def new_resource(self, args: pulumi.runtime.MockResourceArgs):
+        return [args.name + '_id', args.inputs]
+    def call(self, args: pulumi.runtime.MockCallArgs):
+        return {}
+
+pulumi.runtime.set_mocks(MyMocks())
+
+import gcp_storage
+
+@pulumi.runtime.test
+def test_object_in_bucket():
+    def buckets_match(args):
+        bucket_name, source_bucket = args
+        assert bucket_name == source_bucket
+    return pulumi.Output.all(gcp_storage.bucket.name, gcp_storage.bucketObject.bucket).apply(buckets_match)
